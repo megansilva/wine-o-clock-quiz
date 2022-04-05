e6cdf7c7e3fcbdfb02a6c5370491dbebee9073c8
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-04-05 12:30:35 -0400

    adding questions

---
+++ assets/questions.js
@@ -0,0 +1,50 @@
+var questions = [
+    {
+        // question 1
+        title: "Which wine pairs well with salmon?",
+        choices: ["Pinot Grigio", "Chardonnay", "Pinot Noir", "Cabernet"],
+        answer: "Chardonnay"
+    },
+    {
+        // question 2
+        title: "Which wine pairs well with red meat and pasta?",
+        choices: ["Champagne", "Chardonnay", "Cabernet", "Malbec"],
+        answer: "Cabernet"
+    },
+    {
+        // question 3
+        title: "Which wine pairs well with mushrooms and other eathy foods?",
+        choices: ["White Zinfadel", "Pinot Grigio", "Reisling", "Pinot Noir"],
+        answer: "Pinot Noir"
+    },
+    {
+        // question 4
+        title: "Which wine is said to pair well with light seafood dishes?",
+        choices: ["Pinot Grigio", "Merlot", "Pinot Blanc", "Zinfadel"],
+        answer: "Pinot Grigio"
+    },
+    {
+        // question 5
+        title: "Which tpoe of wine paris well with tart flavored foods, such as pickles and suaerkraut?",
+        choices: ["Riesling", "Pinot Blanc", "Cabernet", "White Zinfadel"],
+        answer: "Sauvignon Blanc"
+    },
+    {
+        // question 6
+        title: "Which wine pairs well with sushi?",
+        choices: ["Pinot Noir", "Malbec", "Riesling", "Merlot"],
+        answer: "Riesling"
+    },
+    {
+        // question 7
+        title: "Which wine is said to pair well with salty foods?",
+        choices: ["Rose", "Pinot Grigio", "Pinot Blanc", "Champagne"],
+        answer: "Champagne"
+    },
+    {
+        // question 8
+        title: "Which wine pairs well with sweet-spicy barbecue sauces and foods?",
+        choices: ["Champagne", "Chardonnay", "Cabernet", "Malbec"],
+        answer: "Malbec"
+    },
+]
